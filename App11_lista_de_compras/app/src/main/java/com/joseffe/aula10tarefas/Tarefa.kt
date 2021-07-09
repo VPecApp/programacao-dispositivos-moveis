46938816aad2d26b69aa0047e9c5ccf048a1e25c
MarcosEstevao <junior_estevon.12@hotmail.com>
2021-07-09 14:59:48 -0300

    Cadastrar compras

---
+++ App11_lista_de_compras/app/src/main/java/com/joseffe/aula10tarefas/Tarefa.kt
@@ -0,0 +1,12 @@
+package com.joseffe.aula10tarefas
+
+import androidx.room.*
+
+@Entity(tableName = "TB_TAREFA")
+data class Tarefa(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int=0,
+    @ColumnInfo(name="Nome")
+    val nome: String,
+)
+
